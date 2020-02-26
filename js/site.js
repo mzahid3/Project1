e06e5a8d56ceed18d7b5bde4801d0f5ce000f0c4
mzahid3 <mzahid3@hawk.iit.edu>
2020-02-25 20:51:10 -0600

    commit3

--- js/site.js
+++ js/site.js
@@ -5,7 +5,6 @@ jQuery(function ($) {
     $("html").addClass("hasjs");
    
     });
-const name = document.getElementById('name')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
