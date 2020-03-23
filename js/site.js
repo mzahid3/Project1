646668f8435925531df14a15d82027cd7898679a
mzahid3 <mzahid3@hawk.iit.edu>
2020-03-23 17:27:22 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -5,23 +5,25 @@ jQuery(function ($) {
     $("html").addClass("hasjs");
    
     });
+
+// checking for password 
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
   let messages = []
-
+// checking for less then 6
   if (password.value.length <= 6) {
     messages.push('Password must be longer than 6 characters')
   }
-
+// checking for more then 20 
   if (password.value.length >= 20) {
     messages.push('Password must be less than 20 characters')
   }
 
-
-  if (messages.length > 0) {
+// checking for message
+    if (messages.length > 0) {
     e.preventDefault()
     errorElement.innerText = messages.join(', ')
   }
